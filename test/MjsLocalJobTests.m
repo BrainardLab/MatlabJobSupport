cc18fa87727736faed26e392ea0b883536434621
Ben Heasly <benjamin.heasly@gmail.com>
2016-12-09 16:06:34 -0500

    more tests with local Docker

--- test/MjsLocalJobTests.m
+++ test/MjsLocalJobTests.m
@@ -73,6 +73,96 @@ classdef MjsLocalJobTests < matlab.unittest.TestCase
             messageIndex = strfind(result, expectedMessage);
             testCase.assertNotEmpty(messageIndex);
         end
+        
+        function testInDockerWorkingFolderPathError(testCase)
+            % copy a test function deep inside the working dir.
+            pathHere = fileparts(mfilename('fullpath'));
+            testFunction = fullfile(pathHere, 'fixture', 'mjsJobTestFunction.m');
+            workingDeep = fullfile(testCase.tempDir, 'deep', 'deep', 'deep');
+            mkdir(workingDeep);
+            deepFunction = fullfile(workingDeep, 'deepTestFunction.m');
+            copyfile(testFunction, deepFunction);
+            
+            % "forget" to add the working folder to the path
+            job = mjsJob( ...
+                'pwdPath', false, ...
+                'name', 'needsPwdPath', ...
+                'jobCommand', {@deepTestFunction});
+            status = mjsExecuteLocalJob(job, ...
+                'workingDir', testCase.tempDir);
+            
+            % should fail because mjsJobTestFunction not on Matlab path
+            testCase.assertNotEqual(status, 0);
+        end
+        
+        function testInDockerWorkingFolderPathSuccess(testCase)
+            % copy a test function deep inside the working dir.
+            pathHere = fileparts(mfilename('fullpath'));
+            testFunction = fullfile(pathHere, 'fixture', 'mjsJobTestFunction.m');
+            workingDeep = fullfile(testCase.tempDir, 'deep', 'deep', 'deep');
+            mkdir(workingDeep);
+            deepFunction = fullfile(workingDeep, 'deepTestFunction.m');
+            copyfile(testFunction, deepFunction);
+            
+            % "remember" to add the working folder to the path
+            job = mjsJob( ...
+                'pwdPath', true, ...
+                'name', 'needsPwdPath', ...
+                'jobCommand', {@deepTestFunction});
+            status = mjsExecuteLocalJob(job, ...
+                'workingDir', testCase.tempDir);
+            
+            % should fail because mjsJobTestFunction not on Matlab path
+            testCase.assertEqual(status, 0);
+        end
+        
+        
+        function testInDockerWithLogFile(testCase)
+            job = testCase.integerSaverJob();
+            mjsExecuteLocalJob(job, ...
+                'workingDir', testCase.tempDir, ...
+                'logDir', testCase.tempDir);
+            
+            expectedLogFile = fullfile(testCase.tempDir, 'matlab.log');
+            testCase.assertEqual(exist(expectedLogFile, 'file'), 2);
+            
+            fid = fopen(expectedLogFile, 'r');
+            try
+                logText = fread(fid, '*char')';
+            catch err
+                fclose(fid);
+                rethrow(err);
+            end
+            fclose(fid);
+            
+            expectedMessage = 'Finished job named "integerSaverJob"';
+            messageIndex = strfind(logText, expectedMessage);
+            testCase.assertNotEmpty(messageIndex);
+        end
+        
+        function testInDockerWithCommonToolbox(testCase)
+            % install a toolbox in a shared toolbox folder
+            commonToolboxDir = fullfile(tempdir(), 'toolboxes');
+            tbUse('sample-repo', 'toolboxRoot', commonToolboxDir);
+            
+            % make a job that uses the same toolbox
+            job = testCase.integerSaverJob();
+            job.tbUseArgs = {'sample-repo'};
+            
+            % run the job with the shared toolbox folder mapped in
+            [status, result] = mjsExecuteLocalJob(job, ...
+                'workingDir', testCase.tempDir, ...
+                'commonToolboxDir', commonToolboxDir);
+            
+            testCase.assertEqual(status, 0);
+            
+            % job should find sample-repo from the shared folder /opt/toolboxes,
+            %   not in the internal folder /mjs/toolboxes
+            expectedMessage = 'Adding "sample-repo" to path at "/opt/toolboxes/sample-repo"';
+            messageIndex = strfind(result, expectedMessage);
+            testCase.assertNotEmpty(messageIndex);
+        end
+        
     end
     
 end
---
+++ test/fixture/mjsJobTestFunction.m
@@ -0,0 +1,4 @@
+function mjsJobTestFunction()
+% Function to be called during MatlabJobSupportTests
+
+disp('This is a test function.');
