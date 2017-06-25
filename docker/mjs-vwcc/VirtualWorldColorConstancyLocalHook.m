cbe0487ac5d92dba683b874c5e1ea20a92cd9e76
David Brainard <brainard@psych.upenn.edu>
2017-06-25 11:29:41 -0400

    Updating stuff.
    Signed-off-by: David Brainard <brainard@psych.upenn.edu>

---
+++ docker/mjs-vwcc/VirtualWorldColorConstancyLocalHook.m
@@ -0,0 +1,32 @@
+%% Configure VirtualWorldColorConstancy system defaults.
+%
+% This script attempts to locate system resources and create Matlab
+% preferences necessary to use VirtualWorldColorConstancy.
+%
+% This version is set up for MatlabJobSupport
+
+
+%% Set up some parameters for portability
+projectName = 'VirtualWorldColorConstancy';
+
+%% Where does output go?
+% 
+% The default for output if you are not a recognized user is in a subdir
+% called output/VirtualWorldColorConstancy of what is returned by
+% rtbGetUserFolder, which itself is configured as part of setting up
+% RenderToolbox (there is a default if you don't do anything.)
+%
+% You may want to change this to be wherever you want the potentially big
+% pile of output to end up.
+%
+% We make an attempt below to do sensible things for users/machines we know
+% about before dropping to the default
+
+dataDirRoot = fullfile(rtbGetUserFolder(), 'output');
+dataDirName = 'VirtualWorldColorConstancy';
+dataDir = fullfile(dataDirRoot,projectName,'');
+if (~exist(dataDir,'dir'))
+    mkdir(dataDir);
+end
+setpref(projectName, 'baseFolder',dataDir);
+
